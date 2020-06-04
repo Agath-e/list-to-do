ec55fddb37d8d84981fe2adadf091d42fa55bf75
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-06-04 19:24:37 +0200

    add correct SearchResults

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,33 +2,19 @@ import React from 'react';
 import styles from './SearchResults.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
-import { settings } from '../../data/dataStore';
-import Icon from '../Icon/Icon';
 import Container from '../Container/Container';
 
 class SearchResults extends React.Component {
   static propTypes = {
-    title: PropTypes.string.isRequired,
     cards: PropTypes.array,
-    icon: PropTypes.string,
-    addCard: PropTypes.func,
   };
 
-  static defaultProps = {
-    icon: settings.defaultColumnIcon,
-  };
 
   render() {
-    const { title, icon, cards} = this.props;
+    const { cards} = this.props;
     return (
       <Container>
         <section className={styles.component}>
-          <h3 className={styles.title}>
-            <span className={styles.icon}>
-              <Icon name={icon} />
-            </span>
-            {title}
-          </h3>
           <div>
             {cards.map(cardData => (
               <Card key={cardData.id} {...cardData} />
